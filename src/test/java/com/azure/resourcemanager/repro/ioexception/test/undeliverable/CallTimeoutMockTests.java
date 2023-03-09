7a3c53c2990b1246a83096c11c43c01a7e39a7c4
xiaofeicao <xiaofeicao@microsoft.com>
2023-03-09 17:17:00 +0800

    reproduce IOException: Canceled

--- src/test/java/com/azure/resourcemanager/repro/ioexception/test/undeliverable/CallTimeoutMockTests.java
+++ src/test/java/com/azure/resourcemanager/repro/ioexception/test/undeliverable/CallTimeoutMockTests.java
@@ -3,15 +3,21 @@ package com.azure.resourcemanager.repro.ioexception.test.undeliverable;
 import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.HttpClient;
+import com.azure.core.http.okhttp.OkHttpAsyncHttpClientBuilder;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.resources.models.DeploymentMode;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.matching.MatchResult;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
 import io.reactivex.rxjava3.core.Single;
 import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
@@ -24,16 +30,21 @@ import reactor.core.publisher.Mono;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.time.OffsetDateTime;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.head;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
 public class CallTimeoutMockTests {
     private static final byte[] RESPONSE_BODY = "{\"id\":\"em\",\"name\":\"kzsz\",\"status\":\"IN_PROGRESS\",\"percentComplete\":8.862287,\"startTime\":\"2021-03-07T09:12:02Z\",\"endTime\":\"2021-04-02T00:38:08Z\"}".getBytes(StandardCharsets.UTF_8);
@@ -41,13 +52,15 @@ public class CallTimeoutMockTests {
     private static final String SUBSCRIPTION_ID = UUID.randomUUID().toString();
     private static final String RG_NAME = "my-rg";
     private static final String POLL_URL = "/pollOperation";
+    private static final String DEPLOYMENT_NAME = "my-deployment";
+    private static final int HTTPS_PORT = 54321;
 
     private static WireMockServer server;
 
     @BeforeAll
     public static void beforeClass() {
         server = new WireMockServer(WireMockConfiguration.options()
-                .dynamicHttpsPort()
+                .httpsPort(HTTPS_PORT)
                 .disableRequestJournal()
                 .gzipDisabled(true));
 
@@ -61,8 +74,31 @@ public class CallTimeoutMockTests {
                         // delay response for 10 seconds, so that client always experiences call timeouts
                         .withFixedDelay(10000)));
 
+        server.stubFor(put(String.format(
+                "/subscriptions/%s/resourcegroups/%s/providers/Microsoft.Resources/deployments/%s?api-version=2021-01-01",
+                SUBSCRIPTION_ID,
+                RG_NAME,
+                DEPLOYMENT_NAME))
+                .willReturn(
+                        aResponse()
+                                .withStatus(201)
+                                .withHeader("AzureAsyncOperation", String.format("https://localhost:%d%s", HTTPS_PORT, POLL_URL))
+                                .withBody(RESPONSE_BODY)
+                                // delay response for 10 seconds, so that client always experiences call timeouts
+                                .withFixedDelay(10000)
+                ));
+
+        server.stubFor(head(urlEqualTo(String.format("/subscriptions/%s/resourcegroups/%s/providers/Microsoft.Resources/deployments/%s?api-version=2021-01-01", SUBSCRIPTION_ID, RG_NAME, DEPLOYMENT_NAME)))
+        .willReturn(aResponse()
+                .withStatus(201)
+                .withHeader("AzureAsyncOperation", String.format("https://localhost:%d%s", HTTPS_PORT, POLL_URL))
+                .withBody(RESPONSE_BODY)
+                // delay response for 10 seconds, so that client always experiences call timeouts
+                .withFixedDelay(10000))
+        );
+
         // mock poll endpoint, though create resource group seems not an LRO, never used
-//        server.stubFor(get(POLL_URL).willReturn(aResponse().withStatus(201).withBody(RESPONSE_BODY)));
+        server.stubFor(get(POLL_URL).willReturn(aResponse().withStatus(202).withBody(RESPONSE_BODY).withFixedDelay(10000)));
 
         server.start();
     }
@@ -71,7 +107,7 @@ public class CallTimeoutMockTests {
      * Test entrance.
      */
     @Test
-    public void test() throws NoSuchAlgorithmException, KeyManagementException {
+    public void test() throws NoSuchAlgorithmException, KeyManagementException, InterruptedException {
 
         HttpClient client = buildHttpClient();
         // replace ARM endpoint to our mock host
@@ -87,28 +123,13 @@ public class CallTimeoutMockTests {
                                 new AzureProfile(UUID.randomUUID().toString(), SUBSCRIPTION_ID, AzureEnvironment.AZURE))
                         .withDefaultSubscription();
 
-        AtomicBoolean errorEncountered = new AtomicBoolean(false);
-        AtomicBoolean finished = new AtomicBoolean(false);
+        Thread thread = new Thread(() -> manager.deployments().checkExistence(RG_NAME, DEPLOYMENT_NAME));
 
-        Assertions.assertThrows(Exception.class, () -> {
-            try {
-                ResourceGroup resourceGroup = Single.fromPublisher(manager.resourceGroups()
-                        .define(RG_NAME)
-                        .withRegion(Region.US_WEST)
-                        .createAsync())
-                        // doOnError on Single
-                        .doOnError(throwable -> {
-                            throwable.printStackTrace();
-                            errorEncountered.set(true);
-                        })
-                        .blockingGet();
-            } finally {
-                finished.set(true);
-            }
-        });
+        thread.start();
+        // ensure the thread starts
+        Thread.sleep(1000);
 
-        Assertions.assertTrue(errorEncountered.get());
-        Assertions.assertTrue(finished.get());
+        thread.interrupt();
     }
 
     @NotNull
@@ -144,18 +165,27 @@ public class CallTimeoutMockTests {
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .dispatcher(dispatcher)
-                .addNetworkInterceptor(
+                .addInterceptor(
                         chain ->
-                            chain.proceed(chain.request()))
+                        {
+                            try {
+                                return chain.proceed(chain.request());
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                throw e;
+                            }
+                        }
+                )
                 // set call timeout to 1 second, so that client always experiences call timeouts
-                .callTimeout(Duration.ofSeconds(1))
-//                .readTimeout(Duration.ofSeconds(1))
+                .connectTimeout(Duration.ofSeconds(100))
+                .writeTimeout(Duration.ofSeconds(100))
+                .readTimeout(Duration.ofSeconds(100))
                 // bypass https check
                 .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
                 .hostnameVerifier((hostname, session) -> true)
                 .build();
 
         // this is just a copy of OKHttpAsyncHttpClient to set custom OkHttpClient as inner client
-        return new CustomOkHttpAsyncHttpClient(okHttpClient);
+        return new OkHttpAsyncHttpClientBuilder(okHttpClient).build();
     }
 }
